2add90ee5a1c86e08e5257ca1d821bad19de6814
Reno Greenleaf <greenleaf@tut.by>
2020-05-29 10:49:56 +0300

    JS.

--- quest_engine/settings.py
+++ quest_engine/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'quest_engine', # for globally available stuff
     'conversation',
     'django.contrib.admin',
     'django.contrib.auth',
